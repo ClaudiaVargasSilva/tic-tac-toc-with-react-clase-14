09b0f0eb875cd2f7fc528cf17815b724a69b40b6
ClaudiaVargasSilva <75755572+ClaudiaVargasSilva@users.noreply.github.com>
2021-01-23 16:36:29 -0300

    version 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import { extend } from 'jquery';
 import './App.css';
+import React from 'react';
+import ReactDOM from 'react';
+import Inicio from './componentes/inicio';
+import Game from './componentes/game'
+
 
 function App() {
   return (
+    <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Game />
     </div>
-  );
+    </>
+  )
 }
 
 export default App;
+/**/
